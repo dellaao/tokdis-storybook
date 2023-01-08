c97c64249e6c7cf73fcaf4ca299b663f82105550
dellaao <dellaoctaamelia@gmail.com>
2023-01-08 14:10:04 +0700

    first commit

--- src/stories/Button-tokdis.tsx
+++ src/stories/Button-tokdis.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
    * Is this the principal call to action on the page?
    */
   primary?: boolean;
-  /**
+  /** 
    * What background color to use
    */
   backgroundColor?: string;
